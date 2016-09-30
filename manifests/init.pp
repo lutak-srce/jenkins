fe86542044ebf397507c4c0597eec92d9aa23f9a
Jakov Sosic <jsosic@gmail.com>
2016-09-30 08:59:58 +0200

    Add sysconfig for Jenkins (#1)

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,7 @@ class jenkins (
   $package_ensure = $jenkins::params::package_ensure,
   $java_vendor    = $jenkins::params::java_vendor,
   $java_version   = $jenkins::params::java_version,
+  $java_options   = '-Djava.awt.headless=true',
   ) inherits jenkins::params {
   require yum::repo::jenkins
   require "java::${java_vendor}${java_version}"
@@ -15,6 +16,16 @@ class jenkins (
     ensure  => $package_ensure,
     require => [ Package["java-1.${java_version}.0-${java_vendor}-devel"], File['/etc/yum.repos.d/jenkins.repo'], ],
   }
+
+  file { '/etc/sysconfig/jenkins':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0600',
+    content => template('jenkins/sysconfig.erb'),
+    require => Package['jenkins'],
+  }
+
   service { 'jenkins':
     ensure  => running,
     enable  => true,
